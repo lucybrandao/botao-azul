223e727a6dbc53432f998070ba7d33fedb963dae
Lucy Brandão <lucyjbrandao@gmail.com>
2022-08-12 11:28:25 -0300

    implementação do curso

--- public/app/app.js
+++ public/app/app.js
@@ -1,16 +1,20 @@
-import { handleStatus, log } from './utils/promise-helpers.js';
+import { log, timeoutPromise, retry} from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
+import { notasService as service } from './nota/service.js'; 
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
-const sumItems = code => notas => notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0);
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500), 
+);
+
+const action = operations(() => 
+    retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
+    .then(total => EventEmitter.emit('itensTotalizados', total))
+    .catch(console.log)
+);
 
 document
 .querySelector('#myButton')
-.onclick = () => 
-    fetch('http://localhost:3000/notas')
-    .then(handleStatus)
-    .then(sumItems('2143'))
-    .then(console.log)
-    .catch(console.log);
+.onclick = action;
